fdd24cd18e563b2ee9fea2b67ab9f3c075bd0a70
Nelson <107811017+Byneros@users.noreply.github.com>
2023-07-13 20:53:52 -0500

    commit

--- src/components/docentes/ListarTareas.jsx
+++ src/components/docentes/ListarTareas.jsx
@@ -1,26 +1,65 @@
-import React from 'react';
+/**import React, { useEffect, useState } from 'react';
+// utilizar un libreria ant y httpclient para 
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import mongoose from 'mongoose';
 
-const { Option } = Select;
+// Definir el esquema del modelo
+const dataSchema = new mongoose.Schema({
+    nombre: String,
+    descripcion: String,
+    estado: String,
+    fechalimite: Date
+});
 
-const ListarTareas = () => {
+// Crear el modelo usando el esquema
+const DataModel = mongoose.model('Data', dataSchema);
+
+function DataTable() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Conectar a Mongoose
+        mongoose.connect( 'mongodb+srv://Byneros:0621@cluster0.zrp0lem.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+            .then(() => console.log('Conexión exitosa a MongoDB'))
+            .catch((error) => console.error('Error de conexión a MongoDB:', error));
+
+        // Obtener los datos de la base de datos
+        DataModel.find({})
+            .then((result) => {
+                setData(result);
+            })
+            .catch((error) => console.error('Error al obtener los datos:', error));
+    }, []);
+    const columns: GridColDef[] = [
+        { field: 'nombre', headerName: 'Nombre', width: 130 },
+        { field: 'descripcion', headerName: 'Descripcion', width: 130 },
+        { field: 'estado', headerName: 'Estado', width: 90 },
+        { field: 'fechaLimite', headerName: 'Fecha Limite', type: 'dateTime', width: 90,}
+    ];
 
     return (
-        <div>
-            <h2>Listar Tarea</h2>
+        <div style={{ height: 400, width: '100%' }}>
             <DataGrid
-                rows={rows}
+                rows={data}
                 columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
+                pageSize={5}
                 checkboxSelection
             />
         </div>
     );
-}
+};
+
+const ListarTareas = () => {
+    return (
+        <DataTable />
+    );
+};
 // Exportamos el componente `App` como exportación por defecto de este módulo.
 // Esto significa que otros módulos que importen este archivo obtendrán el componente `App` por defecto.
 export default ListarTareas;
+
+
+*/
