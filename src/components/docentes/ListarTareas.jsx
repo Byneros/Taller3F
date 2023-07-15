96a1ef87daa0d09009943ff35f3f1e8ce3113153
Byneros <ing.nelsonjulio@gmail.com>
2023-07-15 01:08:23 -0500

    commit

--- src/components/docentes/ListarTareas.jsx
+++ src/components/docentes/ListarTareas.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import { useTasks } from '../../services/useTask';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const ListarTarea = () => {
-    const { data: tasks, isLoading, isError, error } = useTasks();
+  const { data: tasks, isLoading, isError, error } = useTasks();
 
-    if (isLoading) {
-        return <span> Cargando...</span>;
-    }
+  if (isLoading) {
+    return <span> Cargando...</span>;
+  }
 
-    if (isError) {
-        return <span>Error: {error.message}</span>;
-    }
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
-    return (
-        <div>
-            <h2>Lista de Tareas</h2>
-            {tasks.map((task) => (
-                <div key={task._id}>
-                    <h2>{task.nombre}</h2>
-                    <p>{task.descripcion}</p>
-                    <p>{task.estado}</p>
-                    <p>{task.fechalimite}</p>
-                </div>
-            ))}
+  return (
+    <div>
+      <h2>Lista de Tareas</h2>
+      {tasks.map((task) => (
+        <div key={task._id}>
+          <h2>{task.nombre}</h2>
+          <p>{task.descripcion}</p>
+          <p>{task.estado}</p>
+          <p>{task.fechalimite}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-export default ListarTarea;
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ListarTarea />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
